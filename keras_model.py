0ba14367176fd3bdb36c4acc293cd33bcf1c0a91
RasmusTorp <s214597@dtu.dk>
2023-02-26 22:35:08 +0100

    model

--- keras_model.py
+++ keras_model.py
@@ -8,7 +8,7 @@ from keras import layers
 from keras import callbacks
 
 class CNN():
-    def __init__(self,dropRate1=0.1,dropRate2=0.1,
+    def __init__(self,dropRate1,dropRate2,input_shape,
     loss_func="sparse_categorical_crossentropy",
     act="relu",opt="adam"):
         self.model = Sequential()
@@ -26,7 +26,7 @@ class CNN():
     def train(self,X_train,y_train,X_test,y_test,batch_size=128,epochs=10,verbose=2):
         self.model.fit(X_train,y_train,batch_size=batch_size,epochs=epochs,validation_data=(X_test,y_test),verbose=verbose)
     
-    def train_opt(self,X_train,y_train,X_test,y_test,batch_size=8,epochs=20,verbose=2):
+    def train_opt(self,X_train,y_train,X_test,y_test,batch_size=128,epochs=20,verbose=2):
         earlystopping = callbacks.EarlyStopping(monitor ="val_loss", 
                                         mode ="min", patience = 3, 
                                         restore_best_weights = True)
