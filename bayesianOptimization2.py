f7d081e0057d3ade8637d38e25c8ac186faf30e7
jesperdlau <jesperdlau@gmail.com>
2023-03-02 17:06:44 +0100

    ændringer

--- bayesianOptimization2.py
+++ bayesianOptimization2.py
@@ -18,25 +18,21 @@ import GPyOpt
 #from scipy.stats import uniform
 #import random
 
-from plot2 import acqfunc, plot_perf, plot_arrow
+from plot2 import plot_perf2, plot_acq_init, plot_acq_full, save_acq_img, load_acq_img
 
 
-X = np.load("image_data_gray.npy")[:200]
-y = np.load("labels.npy")[:200]
-# X = np.load("image_data_gray.npy")
-# y = np.load("labels.npy")
+X = np.load("image_data_gray.npy")[:500]
+y = np.load("labels.npy")[:500]
+X = np.load("image_data_gray.npy")
+y = np.load("labels.npy")
 
 SHAPE = np.shape(X[0])
 seed = 42
-n_iter = 10
+n_iter = 15
 BATCH_SIZE = 100
 EPOCHS = 20
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=seed)
-
-# define the dictionary for GPyOpt
-# domain = [{'name': 'dropRate1', 'type': 'discrete', 'domain': tuple(np.arange(0, 0.5, 0.01))},
-#           {'name': 'dropRate2', 'type': 'discrete', 'domain': tuple(np.arange(0, 0.5, 0.01))}]
 
 domain = [{'name': 'dropRate1', 'type': 'continuous', 'domain': (0.0, 0.5)},
           {'name': 'dropRate2', 'type': 'continuous', 'domain': (0.0, 0.5)}]
@@ -50,41 +46,29 @@ def blackBoxFunction(x):
     loss, acc = model.evaluate(X_test,y_test)   
     #print(f"Acc: {acc}")
     print(dropRate1, dropRate1)
-    return acc
-
+    print(-acc)
+    return - acc
 
 opt = GPyOpt.methods.BayesianOptimization(f = blackBoxFunction,   # function to optimize
                                               domain = domain,         # box-constrains of the problem
                                               acquisition_type = 'EI' ,      # Select acquisition function MPI, EI, LCB
-                                              
-
-
                                              )
 
-opt.acquisition.exploration_weight=0.5
+# opt.acquisition.exploration_weight=0.5
+opt.acquisition.exploration_weight=10
 
-for n in range(n_iter):
-    opt.run_optimization(max_iter = 1, 
+# Random initialization
+opt.run_optimization(max_iter = 0, 
                         verbosity=True,
-                        #report_file="report_file_"+str(n)+".txt",
-                        evaluations_file="eval_file_"+str(n)+".txt", 
-                        #models_file="models_file_"+str(n)+".txt"
-                        ) 
-    acqfunc(opt, "acq_plot_"+str(n)+".png")
+                        evaluations_file="eval_file_iter.txt") 
+save_acq_img(opt, "acq_img_init.npy")
 
-plot_perf(opt, "performance_plot.png")
-plot_arrow("eval_file_"+str(n_iter-1)+".txt", "arrow_plot.png")
+# Iter run
+opt.run_optimization(max_iter = n_iter, verbosity=True,
+                        evaluations_file="eval_file_full.txt", eps = 0.001) 
 
+save_acq_img(opt, "acq_img_full.npy")
+
+# Print output
 x_best = opt.X[np.argmin(opt.Y)]
 print("The best parameters obtained: dropRate1=" + str(x_best[0]) + ", dropRate2=" + str(x_best[1]))
-
-
-
-
-# print(opt.model.input_dim)
-# print(opt.model.get_model_parameters_names())
-# print(opt.model.get_model_parameters())
-
-#acqfunc(opt, "acq.png")
-
-
--- plot2.py
+++ plot2.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from skimage import io
 
 def load_eval(eval_file):
     # Import data
@@ -11,15 +12,21 @@ def load_eval(eval_file):
     df.index = df.index.astype('int8')
     #print(df)
     print("Loaded eval file")
-    
-    return df.round(2)
-    
+    df["acc"] = -1*df["acc"]
+    return df.round(4)
 
+def load_random(random_file):
+    arr = np.load(random_file)
+    df = pd.DataFrame(arr, columns=["Iteration", "acc", "dropRate1", "dropRate2"])
+    df.set_index("Iteration", inplace=True)
+    df.index = df.index.astype('int8')
+    df.index = df.index + 1
+    print("loaded random")
+    return df.round(4)
 
-### Plot acquisition function 2d
-def acqfunc(optimizer, file):
-    xax = np.arange(0, 0.5, 0.01)
-    yax = np.arange(0, 0.5, 0.01)
+def save_acq_img(optimizer, fileout):
+    xax = np.arange(0, 0.5, 0.001)
+    yax = np.arange(0, 0.5, 0.001)
     pgrid = np.array(np.meshgrid(xax, yax,[1],[0],[1],[0],indexing='ij'))
     print(pgrid.reshape(6,-1).T.shape)
     #we then unfold the 4D array and simply pass it to the acqusition function
@@ -28,46 +35,71 @@ def acqfunc(optimizer, file):
     acq_img = (-acq_img - np.min(-acq_img))/(np.max(-acq_img - np.min(-acq_img)))
     #then fold it back into an image and plot
     acq_img = acq_img.reshape(pgrid[0].shape[:2])
+    np.save(fileout, acq_img)
+    print("acquisition img saved to numpy file")
+    #return acq_img
+
+def load_acq_img(filein):
+    acq_img = np.load(filein)
+    return acq_img
+
+
+
+### Plot acquisition function 2d ONLY Random point - with overlay
+def plot_acq_init(acq_file, file, evalfile):
+    xax = np.arange(0, 0.5, 0.001)
+    yax = np.arange(0, 0.5, 0.001)
+    acq_img = load_acq_img(acq_file)
+
+    # Acc data
+    df = load_eval(evalfile)
+    df_len = len(df)
+
+    #init_points = df.iloc[:5]
+    #iter_points = df.iloc[5:]
+
     plt.figure()
     plt.imshow(acq_img.T, origin='lower',extent=[xax[0],xax[-1],yax[0],yax[-1]])
     plt.colorbar()
+
+    # Plot accuracy points
+    sns.scatterplot(data=df, x="dropRate1", y="dropRate2", size="acc", 
+                    sizes=(20, 50), color="red", marker="*", legend=False)
+    # Text labels
+    for i in range(1, df_len+1):
+        plt.text(x=df['dropRate1'][i]+0.002, y=df['dropRate2'][i]+0.002, s=df['acc'][i], 
+                fontdict=dict(color="black", size=10))
+        
     plt.xlabel('dropRate1')
     plt.ylabel('dropRate2')
     plt.title('Acquisition function')
     plt.savefig(file)
     #plt.show()
-    print("saved acq func file")
+    print("saved acq init")
     plt.close()
 
+### Plot acquisition function 2d - with overlay
+def plot_acq_full(acq_file, file, evalfile):
+    xax = np.arange(0, 0.5, 0.001)
+    yax = np.arange(0, 0.5, 0.001)
+    acq_img = load_acq_img(acq_file)
 
-### Plot performance over time
-def plot_perf(opt, file):
-    y_bo = np.maximum.accumulate(opt.Y).ravel()
-    xs = np.arange(1,len(y_bo)+1,1)
-    #plt.plot(xs, max_oob_per_iteration, 'o-', color = 'red', label='Random Search')
-    plt.plot(xs, y_bo, 'o-', color = 'blue', label='Bayesian Optimization')
-    plt.legend()
-    plt.xlabel('Iterations')
-    plt.ylabel('Error')
-    plt.title('Comparison between Random Search and Bayesian Optimization')
-    plt.savefig(file)
-    #plt.show()
-    print("saved performance-over-time plot")
-    plt.close()
-
-
-def plot_arrow(file, fileto):
-    df = load_eval(file)
+    # Acc data
+    df = load_eval(evalfile)
     df_len = len(df)
 
     init_points = df.iloc[:5]
     iter_points = df.iloc[5:]
 
+    plt.figure()
+    plt.imshow(acq_img.T, origin='lower',extent=[xax[0],xax[-1],yax[0],yax[-1]])
+    plt.colorbar()
+
     # Plot accuracy points
     sns.scatterplot(data=init_points, x="dropRate1", y="dropRate2", size="acc", 
-                    sizes=(20, 200), color="red", marker="*", legend=False)
+                    sizes=(10, 50), color="red", marker="*", legend=False)
     sns.scatterplot(data=iter_points, x="dropRate1", y="dropRate2", size="acc", 
-                    sizes=(20, 200), color="black", marker="o", legend=False)
+                    sizes=(10, 50), color="black", marker="x", legend=False)
 
     # Text labels
     for i in range(1, df_len+1):
@@ -79,35 +111,56 @@ def plot_arrow(file, fileto):
         x, y = iter_points['dropRate1'][i], iter_points['dropRate2'][i]
         dx, dy = (iter_points['dropRate1'][i+1] - x)*0.9, (iter_points['dropRate2'][i+1] - y)*0.9
         plt.arrow(x, y, dx, dy, 
-                width = 0.0002, head_width = 0.005)
-
-    # Highlight optimum
-    # for i in [12, 13, 14, 16]:
-    #     x, y = results['droprate_1'][i], results['droprate_2'][i]
-    #     plt.scatter(x = x, y = y,
-    #                 color="blue", marker="^")
-
-    # Kunne være interessant at se Baysian opt mål efter eks. 4, 9 og 10 iters.. 
-
-    plt.xlim(0, 0.51)
-    plt.ylim(0, 0.51)
-    
-    
-    plt.savefig(fileto)
-    print("saved arrow plot plot")
+                width = 0.0002, head_width = 0.005, linestyle='dotted')
+        
+    plt.xlabel('dropRate1')
+    plt.ylabel('dropRate2')
+    plt.title('Acquisition function')
+    plt.savefig(file)
     #plt.show()
-
-    print()
+    print("saved acq full")
     plt.close()
 
 
+### Plot performance over time
+def plot_perf2(filein, random, fileto):
+    df = load_eval(filein)
+    rand = load_random(random)
+    
+    y_bo = np.maximum.accumulate(df['acc']).ravel()
+    y_rand = np.maximum.accumulate(rand['acc']).ravel()
+    max_len = max(len(y_bo), len(y_rand))
+    xs = np.arange(1,max_len+1,1)
+    plt.plot(xs, y_rand, 'o-', color = 'red', label='Random Search')
+    plt.plot(xs, y_bo, 'o-', color = 'blue', label='Bayesian Optimization')
+    plt.legend()
+    plt.xlabel('Iteration')
+    plt.ylabel('Best Accuracy')
+    plt.title('Comparison between Random Search and Bayesian Optimization')
+    plt.savefig(fileto)
+    #plt.show()
+    print("saved performance-over-time plot")
+    plt.close()
+
+
+
 if __name__ == "__main__":
-    plot_arrow("eval_file_4.txt", "arrow_plot.png")
+    #plot_arrow("eval_file_4.txt", "arrow_plot.png")
+    #plot_arrow5("eval_file_4.txt", "arrow_plot5.png")
 
-    plot_perf(opt, "performance_plot.png")
+    # plot_perf2("eval_file_4.txt", "randomSearchResults.npy",  "performance_plot.png")
+
+    #plot_acq_rand()
 
 
+    print()
     #return
+
+    plot_acq_init("acq_img_init.npy", "plot_acq_init.png", "eval_file_iter.txt")
+    plot_acq_full("acq_img_full.npy", "plot_acq_full.png", "eval_file_full.txt")
+    plot_perf2("eval_file_4.txt", "randomSearchResults.npy",  "performance_plot.png")
+
+
 
 
 
